888701de486e8a651f0f8bfbb439c941f10b82f2
Wesley Note Windows <wesleyolieirap@gmail.com>
2023-04-01 18:31:36 -0300

    Implement validations in placeBid

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -1,6 +1,7 @@
 import AWS from 'aws-sdk';
 import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import { getAuctionById } from './getAuction';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -8,6 +9,12 @@ async function placeBid(event, context) {
     let updatedAuction;
     const { id } = event.pathParameters;
     const { amount } = event.body;
+
+    const auction = await getAuctionById(id);
+
+    if (amount <= auction.highestBid.amount) {
+      throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`);
+    }
 
     const params = {
       TableName: process.env.AUCTIONS_TABLE_NAME,
@@ -26,10 +33,6 @@ async function placeBid(event, context) {
     } catch (error) {
         console.error(error);
         throw new createError.InternalServerError(error);
-    }
-
-    if (!updatedAuction) {
-        throw new createError.NotFound(`Auction with id "${id}" not found`);
     }
 
     return {
