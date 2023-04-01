cf871a4a0705431f37425ac2c7fcf82c2b51178a
Wesley Note Windows <wesleyolieirap@gmail.com>
2023-04-01 17:45:57 -0300

    Install middy middleware

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,15 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function crateAuction(event, context) {
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body;
 
   const now = new Date();
 
@@ -15,10 +20,15 @@ async function crateAuction(event, context) {
     createdAt: now.toISOString(),
   };
 
-  await dynamodb.put({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction,
-  }).promise();
+  try {
+    await dynamodb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    }).promise();
+  } catch (error) {
+    console.error(error);
+    throw createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 201,
@@ -26,6 +36,9 @@ async function crateAuction(event, context) {
   };
 }
 
-export const handler = crateAuction;
+export const handler = middy(crateAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
 
 
