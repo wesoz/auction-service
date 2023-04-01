83a8179ea2afcfe707178bddd1fb1e9a3fdf9c2b
Wesley Note Windows <wesleyolieirap@gmail.com>
2023-04-01 18:13:42 -0300

    Implement common middleware

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,9 +1,6 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
-import middy from '@middy/core';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
+import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -36,9 +33,6 @@ async function crateAuction(event, context) {
   };
 }
 
-export const handler = middy(crateAuction)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(crateAuction);
 
 
