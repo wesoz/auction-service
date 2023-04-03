a3d60110e908ec55d54690283eec23e81c171c64
Wesley Note Windows <wesleyolieirap@gmail.com>
2023-04-03 14:24:37 -0300

    Implement parameter validation for getAuctions

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,6 +1,8 @@
 import AWS from 'aws-sdk';
 import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -33,6 +35,14 @@ async function getAuctions(event, context) {
     };
 }
 
-export const handler = commonMiddleware(getAuctions);
-
+export const handler = commonMiddleware(getAuctions)
+    .use(
+        validator({
+            inputSchema: getAuctionsSchema,
+            ajvOptions: {
+                useDefaults: true,
+                strict: false,
+            },
+        })
+    );
 
